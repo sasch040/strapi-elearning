313552fa2b3d568aa7c2a42ed34df831777c6893
sasch040 <ftown4447@gmail.com>
2025-07-31 21:33:03 +0200

    Strapi Projekt mit S3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,27 +1,15 @@
-const { S3Client } = require('@aws-sdk/client-s3');
-const { Upload } = require('@strapi/provider-upload-aws-s3');
-
-module.exports = ({ env }) => {
-  const s3Client = new S3Client({
-    region: env('AWS_REGION'),
-    credentials: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-    },
-    maxAttempts: 3, // AWS SDK-integrierte Retry-Anzahl
-  });
-
-  return {
-    upload: {
-      config: {
-        provider: '@strapi/provider-upload-aws-s3',
-        providerOptions: {
-          s3Client, // übergib den Client direkt mit Retry
-          params: {
-            Bucket: env('AWS_BUCKET'),
-          },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
         },
       },
     },
-  };
-};
+  },
+});
