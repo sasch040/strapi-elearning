3c176a2d57485a286ba84adc58951634917ff31e
sasch040 <ftown4446@gmail.com>
2025-08-05 16:14:47 +0200

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  allowedRedirectUrls: [
+    'https://v0-e-learning-platform-design-seven.vercel.app',
+    'https://v0-e-learning-platform-desig-git-f3e374-alexs-projects-db7eed2f.vercel.app',
+    'https://v0-e-learning-platform-desig-git-c19411-alexs-projects-db7eed2f.vercel.app', // optional für Tests
+  ],
 });
